020bbd982761aa7c86df63ab1e19eec13ddc47b8
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-09-12 09:01:35 -0300

    Relational object mapping of entities

--- projeto-springboot-rest-api/src/main/java/com/engfabiorogerio/projetospringboot_restapi/entities/User.java
+++ projeto-springboot-rest-api/src/main/java/com/engfabiorogerio/projetospringboot_restapi/entities/User.java
@@ -1,10 +1,26 @@
 package com.engfabiorogerio.projetospringboot_restapi.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
+	
+	@ManyToOne
+	@JoinColumn(name = "department_id")
+	private Department department;
 
 	public User() {
 		// TODO Auto-generated constructor stub
