1f60795a57db3edaa2f4601053aa258b9572ca3f
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-09-12 08:54:46 -0300

    Create model fields

--- projeto-springboot-rest-api/src/main/java/com/engfabiorogerio/projetospringboot_restapi/entities/Department.java
+++ projeto-springboot-rest-api/src/main/java/com/engfabiorogerio/projetospringboot_restapi/entities/Department.java
@@ -2,4 +2,27 @@ package com.engfabiorogerio.projetospringboot_restapi.entities;
 
 public class Department {
 
+	private Long id;
+	private String nome;
+
+	public Department() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
--- projeto-springboot-rest-api/src/main/java/com/engfabiorogerio/projetospringboot_restapi/entities/User.java
+++ projeto-springboot-rest-api/src/main/java/com/engfabiorogerio/projetospringboot_restapi/entities/User.java
@@ -2,4 +2,36 @@ package com.engfabiorogerio.projetospringboot_restapi.entities;
 
 public class User {
 
+	private Long id;
+	private String nome;
+	private String email;
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
